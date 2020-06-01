e221831abd8ea194ec5d580dd3f9349246a709d2
Robert Kaye <rob@musicbrainz.org>
2020-06-01 22:28:48 +0200

    Add json support

--- datasethoster/app.py
+++ datasethoster/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from werkzeug.exceptions import NotFound
 
 registered_queries = {}
@@ -19,7 +19,7 @@ def index():
     return render_template("index.html", queries=registered_queries)
 
 
-def query_handler():
+def web_query_handler():
     slug = request.path[1:]
     try:
         query = registered_queries[slug]
@@ -46,8 +46,33 @@ def query_handler():
                            slug=slug)
 
 
+def json_query_handler():
+    slug, _ = request.path[1:].split("/")
+    try:
+        query = registered_queries[slug]
+    except KeyError:
+        raise NotFound
+
+    slug, desc = query.names()
+    inputs = query.inputs()
+    columns = query.outputs()
+    args = {}
+    for input in inputs:
+        try:
+            args[input] = request.args[input]
+        except KeyError:
+            raise NotFound("Required argument %s missing." % input)
+        if not args[input]:
+            raise NotFound("Required argument %s cannot be blank." % input)
+
+
+    data = query.fetch(args)
+    return jsonify(data)
+
+
 def register_query(query):
 
     slug, name = query.names()
     registered_queries[slug] = query
-    app.add_url_rule('/%s' % slug, slug, query_handler)
+    app.add_url_rule('/%s' % slug, slug, web_query_handler)
+    app.add_url_rule('/%s/json' % slug, slug + "_json", json_query_handler)
