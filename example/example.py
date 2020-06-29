97cbab1a4e942bc898d46cb95c163f643a2c2300
Robert Kaye <rob@musicbrainz.org>
2020-06-29 10:45:25 +0200

    Add a note about the JSON version

--- example/example.py
+++ example/example.py
@@ -40,4 +40,4 @@ class ExampleQuery(Query):
 
 if __name__ == "__main__":
     register_query(ExampleQuery())
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8001, debug=True)
