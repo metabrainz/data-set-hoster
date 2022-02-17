d9ebb87f4a683d0c0a2c5e177c71193f118045c5
Robert Kaye <rob@musicbrainz.org>
2022-02-17 17:05:07 +0100

    Limit to 50 tracks.

--- datasethoster/main.py
+++ datasethoster/main.py
@@ -218,7 +218,7 @@ def web_query_handler():
                            slug=slug,
                            json_url=json_url,
                            json_post=json_post,
-                           recording_mbids=",".join(recording_mbids[:100]))
+                           recording_mbids=",".join(recording_mbids[:50]))
 
 
 @crossdomain(headers=["Content-Type"])
