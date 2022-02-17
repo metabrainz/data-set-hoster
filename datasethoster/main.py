45b4ba304fe5e7cabe3ecfbc407bc17bf0e53e3b
Robert Kaye <rob@musicbrainz.org>
2022-02-17 16:31:39 +0100

    Cast recording_mbid to str just in case

--- datasethoster/main.py
+++ datasethoster/main.py
@@ -202,7 +202,7 @@ def web_query_handler():
                             pass
 
                     if output == "recording_mbid":
-                        recording_mbids.append(arg["recording_mbid"])
+                        recording_mbids.append(str(arg["recording_mbid"]))
 
 
     json_url = request.url.replace(slug, slug + "/json")
