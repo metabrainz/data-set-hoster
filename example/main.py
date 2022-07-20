649427f550c489fbc4b025ee9ae4b05b4ae456f4
Alastair Porter <alastair@metabrainz.org>
2022-07-20 16:09:52 +0200

    Fix example query

--- example/main.py
+++ example/main.py
@@ -2,9 +2,10 @@ from datasethoster.main import create_app, register_query
 
 from example import ExampleQuery
 
-app = create_app('config')
-
 register_query(ExampleQuery())
+
+# Needs to be after register_query
+app = create_app('config')
 
 if __name__ == "__main__":
     # Only for debugging while developing
