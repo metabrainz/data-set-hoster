4d38b9ddaf6aac5240d00447e24b195f4cbd7796
Vishal <cvishalftw@gmail.com>
2023-08-10 20:06:59 +0530

    Queries to be used in dropdown for similar results

--- example/example.py
+++ example/example.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
-import json
 from datetime import datetime
 from enum import Enum
-from typing import List, Union, Optional
-from uuid import UUID
+from typing import List
 
-from markupsafe import Markup
 from pydantic import BaseModel
 
-from datasethoster import Query, RequestSource, QueryOutputLine
+from datasethoster import Query
 
 
 class ExampleOutput(BaseModel):
     number: int
-    num_lines: int
-    x: str
+    multiplied: int
+
+
+class ExampleInput2(BaseModel):
+    number: int
+    multiplied: int
+
+class ExampleOutput2(BaseModel):
+    number: int
+    added: int
 
 
 class ExampleQuery(Query[BaseModel, ExampleOutput]):
@@ -42,7 +47,6 @@ class ExampleQuery(Query[BaseModel, ExampleOutput]):
         return ExampleOutput
 
     def fetch(self, params: list, source, offset=-1, count=-1) -> List[ExampleOutput]:
-        print(params)
         data = []
         for param in params:
             number = param.number
@@ -53,4 +57,34 @@ class ExampleQuery(Query[BaseModel, ExampleOutput]):
                     'option': param.x
                 })
         outputs = [ExampleOutput(number=x["number"], num_lines=x["multiplied"], x=x["option"].value) for x in data]
+        return outputs
+
+
+class ExampleQuery2(Query[ExampleInput2, ExampleOutput2]):
+
+    def setup(self):
+        pass
+
+    def names(self):
+        return "example-2", "Useless arithmetic addition example"
+
+    def introduction(self):
+        return """This is the introduction, which could provide more useful info that this introduction does."""
+
+    def inputs(self):
+        return ExampleInput2
+
+    def outputs(self):
+        return ExampleOutput
+
+    def fetch(self, params: List[ExampleInput2], source, offset=-1, count=-1) -> List[ExampleOutput2]:
+        data = []
+        for param in params:
+            number = param.number
+            for i in range(1, param.multiplied + 1):
+                data.append({
+                    'number': i,
+                    'added': i + number
+                })
+        outputs = [ExampleOutput2(number=x["number"], added=x["added"]) for x in data]
         return outputs
