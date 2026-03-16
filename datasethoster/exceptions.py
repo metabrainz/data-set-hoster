1a9ece6428376b24f60089448a760999adeb4e2c
Kartik Ohri <kartikohri13@gmail.com>
2026-03-17 02:08:13 +0530

    Add QueryError

--- datasethoster/exceptions.py
+++ datasethoster/exceptions.py
@@ -6,4 +6,17 @@ class RedirectError(Exception):
         super().__init__()
 
 class QueryError(Exception):
-    """ An error occured during the execution of the query and the error should be shown to the user. """
+    """Raise from a Query's fetch() method to return a user-facing error.
+
+    For web views, the message is displayed on the error page. For JSON API
+    views, the response body is {"error": message, "code": status_code}.
+
+    Args:
+        message: A human-readable error description shown to the user.
+        status_code: The HTTP status code for the response (default 400).
+    """
+
+    def __init__(self, message: str = None, status_code: int = 400):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(message)
